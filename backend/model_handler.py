5772e34a343230d76de0ee6dbe211b8ccd171f1b
anthonyFalter <rosselopre.business@gmail.com>
2026-08-31 11:47:39 +0800

    initialize __init__ for model_handler

--- backend/model_handler.py
+++ backend/model_handler.py
@@ -5,3 +5,12 @@ import pandas as pd
 from backend.schemas import ChurnInputSchemas
 
 logger = logging.getLogger(__name__)
+
+class ModelHandler:
+    def __init__(self, model_path: Path):
+        self.model_path = model_path
+        self.model = None
+        self.is_loaded = False
+        self.load_model()
+        
+    
