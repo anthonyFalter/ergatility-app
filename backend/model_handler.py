878617a58cb5ebbdf5e8848ea51a7c70848b1275
anthonyFalter <rosselopre.business@gmail.com>
2026-08-31 11:57:11 +0800

    initialize base features dictionary for model_handler

--- backend/model_handler.py
+++ backend/model_handler.py
@@ -34,4 +34,22 @@ class ModelHandler:
         
         raw_dict = data.model_dump()
         
+        # Map Ordinal Salary
+        salary_map = {'low': 0, 'medium': 1, 'high': 2}
+        salary_val = salary_map.get(raw_dict.pop("salary"),1)
+        
+        # Base Features dictionary
+        processed_data = {
+            'satisfaction_level': raw_dict['satisfaction_level'],
+            'last_evaluation': raw_dict['last_evaluation'],
+            'number_project': raw_dict['number_project'],
+            'average_montly_hours': raw_dict['average_montly_hours'],
+            'time_spend_company': raw_dict['time_spend_company'],
+            'work_accident': raw_dict['work_accident'],
+            'promotion_last_5years': raw_dict['promotion_last_5years'],
+            'salary': salary_val,
+                                             
+        }
+        
+        # Apply One-Hot Encoding for `Departments`
         
