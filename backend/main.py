a40bb7376718db2bc136204c982e43eb7a3115e4
anthonyFalter <rosselopre.business@gmail.com>
2026-08-31 11:22:38 +0800

    update CORS middleware params'

--- backend/main.py
+++ backend/main.py
@@ -26,6 +26,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Authorization", "Content-Type"],
 )
